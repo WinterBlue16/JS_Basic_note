f703a75f70fc85958114b0dcc3b122bf5d949e06
WinterBlue16 <leekh090163@gmail.com>
2022-03-22 23:32:24 +0900

    uppercase lowercase

---
+++ 12_uppercase_lowercase.js
@@ -0,0 +1,8 @@
+let sampleStringLow = "hello javascript!";
+let sampleStringUp = "HELLO JAVASCRIPT!";
+
+//uppercase
+console.log(sampleStringLow.toUpperCase()); 
+
+//lowercase
+console.log(sampleStringUp.toLowerCase());
