fdd227b81abcdf161f7839281d2da382be4eb4cd
WinterBlue16 <leekh090163@gmail.com>
2022-03-24 00:01:39 +0900

    check string contain korean text

--- 13_check_regex.js
+++ 13_check_regex.js
@@ -1,5 +1,7 @@
-sampleString = "https://www.naver.com"
+const sampleString = "https://www.naver.com";
+const sampleStringKorean = "안녕 javascript!";
 
 // 한글 검사
-const korean = /[]/;
-korean.test(sampleString);
+const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+console.log(korean.test(sampleString)); //false
+console.log(korean.test(sampleStringKorean)); //true
