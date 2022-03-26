04218f790f5448face52ed4e0e934bf9a69d0bda
WinterBlue16 <leekh090163@gmail.com>
2022-03-27 00:07:06 +0900

    extract obj's key & values

---
+++ 16_extract_keys_values_in_obj.js
@@ -0,0 +1,10 @@
+let sampleObject = {};
+sampleObject['apple'] = 1;
+sampleObject['grape'] = 2;
+sampleObject['strawberry'] = 3;
+
+// keys
+console.log(Object.keys(sampleObject));
+
+// values
+console.log(Object.values(sampleObject));
