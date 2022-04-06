f8d34623f91e01f3ecaaa4f376050d1ca109cfdf
WinterBlue16 <leekh090163@gmail.com>
2022-04-06 23:49:16 +0900

    if else shorthand

---
+++ 17_if_else.js
@@ -0,0 +1,8 @@
+// single condition 
+const number = 10;
+let sample1 = (number > 10) ? 10 : 11;
+console.log(sample1);
+
+// multiple condition
+let sample2 = (number > 10) ? 10 : ((number < 5) ? 9 : 5);
+console.log(sample2);
