a732e0070e1fd98a2af4f292ba9524e7aa3df93e
Winter <winter@glocalizeinc.com>
2022-06-16 23:54:13 +0900

    [MOD]remove word boundary

--- 31_regex_word_boundary.js
+++ 31_regex_word_boundary.js
@@ -5,3 +5,6 @@ console.log(textWithoutWordBoundary.match(/^hello/)); //match
 console.log(textWithWordBoundary.match(/^hello/)); //null
 
 console.log(textWithoutWordBoundary.match(/\b[a-z][a-z][a-z][a-z][a-z]/)); //match
+
+// remove word boundary
+console.log(textWithWordBoundary.replaceAll("\b", "").match(/^hello/));
